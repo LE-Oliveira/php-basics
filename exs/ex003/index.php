9d03dda1739b0fcce2847bbd107687d5c023bd36
LE-Oliveira <aber00fort@gmail.com>
2024-08-12 08:10:29 -0300

    Did some basic exercises

---
+++ exs/ex003/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php 
+        $num = 0x1A;
+        echo "O valor da variável numérica é $num";
+        echo "<br>";
+
+        $v = (integer) "1457";
+        var_dump($v);
+        echo "<br>";
+        
+        $booleana = false;
+        echo "A variável booleana é $booleana";
+        echo "<br>";
+
+        $vet = [6, 1, 8, 3, 6];
+        var_dump($vet);
+        echo "<br>";
+
+        class Pessoa {
+            private string $nome;
+            function __construct($teste) {
+                $this->nome = $teste;
+            }
+        }
+        $p = new Pessoa("Lucas");
+        var_dump($p);;
+    ?>
+</body>
+</html>
